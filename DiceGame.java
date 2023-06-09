116616983fb2301c25e9b7a5ac69069e74ca9bc3
s1300092 <syoutokutaishi2016@outlook.jp>
2023-06-09 12:15:17 +0900

    Won/lost message modified

--- DiceGame.java
+++ DiceGame.java
@@ -1,18 +1,24 @@
 import java.util.Random;
+import java.util.Scanner;
 
 class DiceGame{
     public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
 	Random rand = new Random();
 	int a = (rand.nextInt(6) + 1);
 	int b = (rand.nextInt(6) + 1);
-	System.out.println("Rolling dice...");
+	System.out.println("What is your name?");
+	System.out.println("> ");
+	String name = sc.next();
+	System.out.println("Hello, " + name + "!");
+	System.out.println("Rolling the dice...");
 	System.out.println("Die 1: " + a);
 	System.out.println("Die 2: " + b);
 	System.out.println("Total value: " + (a+b));
 	if((a+b) > 7){
-	    System.out.println("You won.");
+	    System.out.println(name + " won.");
 	}else{
-	    System.out.println("You lost.");
+	    System.out.println(name + " lost.");
 	}
     }
 }
